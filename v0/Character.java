fcab9f5d342a2d256d741687238f050ff08d6634
Nina Jiang <njiang20@stuy.edu>
2021-11-22 14:04:47 -0500

    rpg

---
+++ v0/Character.java
@@ -0,0 +1,39 @@
+public class Character {
+
+    // ~~~~~~~~~~~ INSTANCE VARIABLES ~~~~~~~~~~~
+    private int _hitPts;
+    private int _strength;
+    private int _defense;
+    private double _attack;
+
+    public Character() {
+      _hitPts = 125;
+      _strength = 100;
+      _defense = 40;
+      _attack = .4;
+    }
+
+    public boolean isAlive() {
+         return _hitPts > 0;
+     }
+
+    public int getDefense() { return _defense; }
+
+    public void lowerHP( int damageInflicted ) {
+      _hitPts = _hitPts - damageInflicted;
+    }
+
+    public int attack( Character opponent ) {
+
+      int damage = (int)( (_strength * _attack) - opponent.getDefense() );
+      //System.out.println( "\t\t**DIAG** damage: " + damage );
+
+      if ( damage < 0 )
+         damage = 0;
+
+      opponent.lowerHP( damage );
+
+      return damage;
+      }//end attack
+
+}

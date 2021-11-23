689666a77bac81061a946e95aa75a1b1a8bb17e4
Nina Jiang <90851568+njiang20@users.noreply.github.com>
2021-11-22 20:45:41 -0500

    rpg

--- v0/Protagonist.java
+++ v0/Protagonist.java
@@ -1,45 +1,3 @@
-/* public class Protagonist {
-	private String name;
-	private boolean isAlive = true;
-	public int health = 100;
-	private int attack = 7;
-
-	public Protagonist (String name ) {
-		this.name = name;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public boolean isAlive() {
-		if (health > 0) {
-			isAlive = true;
-		} else {
-			isAlive = false;
-		}
-		return isAlive;
-	}
-
-	public void normalize() {
-		attack = 7;
-	}
-
-	public void specialize() {
-		attack = 15;
-	}
-
-	public int attack(Monster m) {
-		m.health -= attack;
-		return attack;
-	}
-}
-*/
-
-/**
-  class Protagonist -- protagonist of Ye Olde RPG
-  **/
-
 public class Protagonist extends Character {
 
     // ~~~~~~~~~~~ INSTANCE VARIABLES ~~~~~~~~~~~
@@ -77,7 +35,7 @@ public class Protagonist extends Character {
 
     // ~~~~~~~~~~~~~~ ACCESSORS ~~~~~~~~~~~~~~~~~
     public String getName() { return _name; }
-
+/*
     public int getDefense() { return _defense; }
     // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
@@ -120,7 +78,7 @@ public class Protagonist extends Character {
     public void lowerHP( int damageInflicted ) {
         _hitPts = _hitPts - damageInflicted;
     }
-
+*/
 
     //prepare a Protagonist for a special attack
     public void specialize() {

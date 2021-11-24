9ecb03a9014b47afa2b1ba26ffbe909a1d8630df
Nina Jiang <90851568+njiang20@users.noreply.github.com>
2021-11-23 20:35:47 -0500

    rpg

--- v2/Archer.java
+++ v2/Archer.java
@@ -7,24 +7,7 @@ public class Archer extends Protagonist{
     attack = .4;
   }
 
-  public int attack(Character e) {
-    calcNewDamage(this);
-    if (attackState.equals("specialize")) {
-      //System.out.println("Swinging Hammer...");
-      int amplifier = (int) (Math.random() * 10);
-      if (amplifier > 5) {
-        this.health += 20;
-        System.out.println("Youg hath used shield of holy powers!");
 
-      } else {
-        System.out.println("Youg swung too hard, and bashed thy skull. Ye lost 5 health!");
-        lowerHP(5);
-
-      }
-    }
-    e.lowerHP(this.damage);
-    return this.damage;
-  }
 
   public int attack(Character opponent) {
 
